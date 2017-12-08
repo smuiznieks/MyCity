a068e0a54dfe102817645c731361a5bf98d5ae55
Ashlee Brolly <ashleebrolly@Ashlees-MacBook-Pro.local>
2017-12-07 21:34:19 -0500

    updated JS

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -9,34 +9,44 @@ var config = {
 
 firebase.initializeApp(config);
 var database = firebase.database();
-var favePlace;
-var newPlace;
-var service;
-var placeId;
-
-var queryURL = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + placeId + 'ChIJN1t_tDeuEmsRUsoyG83frY4&key=AIzaSyC519iOJi8tbM45WPdfph3XP-nxkdtez-o'
 
 
 // idk if 'child_added' is the correct code to use here but it works so I guess we can leave it?
 database.ref().on('child_added', function(snapshot) {
-	// Finds Place ID and pushes it to the faveArray
-	var savedPlace = snapshot.val();
-	favePlace = savedPlace.id;
-	var request = {
+
+var favePlace;
+var newPlace;
+var service;
+var savedPlace = snapshot.val();
+var favePlace = savedPlace.id;
+var request = {
   		placeId: favePlace
 	};
 
+var queryURL = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + request.placeId + '&key=AIzaSyC519iOJi8tbM45WPdfph3XP-nxkdtez-o'
+
+
+	console.log(queryURL);
+
+
 	}, function(errorObject) {
 		console.log("The read failed: " + errorObject.code);
-});
 
-
- $.ajax({
+ 	$.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-          $("#movie-view").text(JSON.stringify(response));
+        	console.log(response.id);
+        	$('#favorites').text(response.id);
         });
+
+
+});
+
+$('#map').hide();
+
+
+
 
 
 
