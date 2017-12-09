037d7bac8c46cc2e506d40699055b7d94ab4f9c3
Ashlee Brolly <ashleebrolly@Ashlees-MacBook-Pro.local>
2017-12-09 10:01:59 -0500

    messing with Google Places

--- assets/javascript/profile.js
+++ assets/javascript/profile.js
@@ -14,33 +14,44 @@ var database = firebase.database();
 // idk if 'child_added' is the correct code to use here but it works so I guess we can leave it?
 database.ref().on('child_added', function(snapshot) {
 
-var favePlace;
-// var newPlace;
-// var service;
-var savedPlace = snapshot.val();
-var favePlace = savedPlace.id;
-var request = {
-  		placeId: favePlace
-	};
+    var favePlace;
+    // var newPlace;
+    // var service;
+    var savedPlace = snapshot.val();
+    var favePlace = savedPlace.id;
+    var request = {
+        placeId: favePlace
+    };
 
-var queryURL = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + request.placeId + '&key=AIzaSyDMqNQ9pA7C5sKkMHm8U6BAdExqtprHAwE'
+    var queryURL = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + request.placeId + '&key=AIzaSyDMqNQ9pA7C5sKkMHm8U6BAdExqtprHAwE'
 
 
 	console.log(queryURL);
 
 
-	}, function(errorObject) {
-		console.log("The read failed: " + errorObject.code);
+    $.ajax({
+        url: queryURL,
+        type: "GET"
+    }).done(function(response) {
 
- 	$.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-        	
-          console.log(response);
-       
+        console.log(response)
+        
+        // $.ajax({
+        //     url: queryURL, 
+        //     type: "GET",   
+        //     dataType: 'json',
+        //     cache: false,
+        //     }).done(function(response) {                         
+        //         console.log(response);                   
+        //         })            
+    
+
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
         });
-});
+})
+
+
 
 // $('#map').hide();
 
